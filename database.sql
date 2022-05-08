3f677919039d1b82ae05cb0c29f30bf4d94f553c
pedrolmr <moverapedro@gmail.com>
2022-05-08 06:23:47 -0400

    app returns a JSON object with the key “return_string” and a string containing every third letter from the original string

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE words
+
+CREATE TABLE word(
+  word_id SERIAL PRIMARY KEY,
+  string_to_cut VARCHAR(255),
+  return_string VARCHAR(255)
+);
---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const pool = require("./db");
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/test", async (req, res) => {
+  try {
+    const allWords = await pool.query('SELECT * FROM word');
+
+    res.json(allWords.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+
+function everyThreeLetter(string){
+    let str = ""
+    for(let i = 2; i < string.length; i += 3){
+      str += string[i]
+    }
+    return str;
+}
+
+app.post("/test", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { string_to_cut } = req.body;
+    const return_string = everyThreeLetter(string_to_cut);
+    
+    const newWord = await pool.query(
+      'INSERT INTO word (string_to_cut, return_string) VALUES ($1, $2) RETURNING *',
+      [string_to_cut, return_string]
+    );
+
+    const id = newWord.rows[0].word_id;
+    const single_word = await pool.query("SELECT return_string FROM word WHERE word_id = $1", [
+        id,
+    ]);
+
+    res.json(single_word.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is starting on port ${PORT}`);
+});
