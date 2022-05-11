1fa0a8d899e3f97bfb79035854b216855faba07c
pedrolmr <moverapedro@gmail.com>
2022-05-11 06:03:04 -0400

    new update

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();
+import 'dotenv/config';
 
 const devConfig = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const proConfig = process.env.DATABASE_URL;
