3f4de3efb7d55d17041941dc1012fe06a39ecaf9
pedrolmr <moverapedro@gmail.com>
2022-05-11 07:00:43 -0400

    deploy fix

--- db.js
+++ db.js
@@ -14,8 +14,8 @@ const proConfig = {
     connectionString: process.env.DATABASE_URL
 };
 
-const pool = new Pool(
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
-);
+console.log('db url', process.env.DATABASE_URL)
+
+const pool = new Pool(process.env.DATABASE_URL);
 
 module.exports = pool;
